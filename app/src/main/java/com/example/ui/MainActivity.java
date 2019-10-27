587ac94f45e9cf9d27ebda3e1efb08e5d7b62b19
Ion Balan <balan1541@gmail.com>
2019-10-27 11:27:03 -0400

    Adjusted frontpage to physical device

--- app/src/main/java/com/example/ui/MainActivity.java
+++ app/src/main/java/com/example/ui/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startSettings(View view){
+        Intent settings = new Intent(getApplicationContext(),SettingsActivity2.class);
+        startActivity(settings);
+    }
 }
+
